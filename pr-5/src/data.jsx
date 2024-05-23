e7c83c87a2ac4f8ff19727060d6f0e1812c390cc
yogeshnarola01 <yogeshnarola001@gmail.com>
2024-05-23 18:20:35 +0530

    Pr-5

--- pr-5/src/data.jsx
+++ pr-5/src/data.jsx
@@ -123,26 +123,33 @@ const data2 = [
         rprice : '22.10'
     },{
         id : '5',
-        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/1.jpg',
-        name : 'Fresh organic villa farm lomon',
-        category : 'Vegitable',
+        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/11.jpg',
+        name : 'Sweet crunchy nut mix 250gm pack',
+        category : 'Snack',
         price : '120',
         rprice : '123.25'
     },{
         id : '6',
-        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/1.jpg',
-        name : 'Fresh organic villa farm lomon',
-        category : 'Vegitable',
+        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/3.jpg',
+        name : 'Organic fresh venila farm watermelon 5kg',
+        category : 'Fruit',
         price : '120',
         rprice : '123.25'
     },{
         id : '7',
-        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/1.jpg',
-        name : 'Fresh organic villa farm lomon',
-        category : 'Vegitable',
-        price : '120',
+        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/10.jpg',
+        name : 'Sweet crunchy nut mix 250gm pack',
+        category : 'Snack',
+        price : '120.25',
         rprice : '123.25'
+    },{
+        id : '8',
+        img : 'https://maraviyainfotech.com/projects/carrot/carrot-v2/carrot-html/assets/img/product/17.jpg',
+        name : 'Delicious white baked fresh bread',
+        category : 'Bakery',
+        price : '20',
+        rprice : '22.10'
     }
 ]
 
-export {category,data}
+export {category,data,category2,data2}
--- pr-5/src/main/Section.jsx
+++ pr-5/src/main/Section.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { category,data } from '../data'
+import { category,data,category2,data2 } from '../data'
 import { useState } from 'react'
 
 const Section = () => {
   const [record,setRecord] = useState(data)
+  const [record2,setRecord2] = useState(data2)
+
   const FilterData = (item)=>{
     let datas = data.filter((val)=> val.category === item);
     setRecord(datas.slice(0, 2))
-}
+  }
+
+  const FilterData2 = (item)=>{
+    let datas2 = data2.filter((val)=> val.category === item);
+    setRecord2(datas2.slice(0, 2))
+  }
 
   return (
     <div className="container">
@@ -22,14 +29,46 @@ const Section = () => {
                             })
                         }
                 </div>
-                <div class="col-8 d-flex">
+                <div class="col-8 d-flex p-2">
                     {
                         record.map((val)=>{
                         return(
                             <div>
-                                <div class="col-6 picv" key={val.id}>
+                                <div class="col-6 m-1" key={val.id}>
                                     <img src={val.img} />
                                     <button>Shop Now</button>
+                                </div>
+                            </div>
+                        )
+                        })
+                    }
+                </div>
+            </div>
+
+                <br></br><br></br>
+
+            <div class="col-12 d-flex">
+                <div class="col-4 cet">
+                {
+                    category2.map((val)=>{
+                        return(
+                        <button key={val.id} onClick={()=> FilterData2(val.name)} className="btn">{val.name}</button>
+                        )
+                    })
+                }
+                </div>
+                <div class="col-8 d-flex p-2">
+                {
+                        record2.map((val)=>{
+                        return(
+                            <div>
+                                <div className="card m-2" style={{width: '18rem'}}>
+                                    <img src={val.img} className="card-img-top" />
+                                    <div className="card-body">
+                                        <p className="card-text">{val.category}</p>
+                                        <h5 className="card-title">{val.name}</h5>
+                                        <a href="#" className="btn btn-primary ">Shop Now</a>
+                                    </div>
                                 </div>
                             </div>
                         )
